2219b714280b7c8c557e36c6c49151eb7a48cf0f
leoperezx <leoperez.x@gmail.com>
2021-08-18 21:17:09 -0500

    actualizando archivo git-update.sh

--- git-update.sh
+++ git-update.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-# demostración de como leer un texto del usuario
+# Algoritmo para realizar actualización de todo los 
+# archivos del proyecto en el repo del Git. y guarda
+# en un .txt los ultimos archivos modificados.  
 
 echo "texto del commit?"
 read text_commit
-
 echo "Últimos archivos modificados hace 5 horas son: "
-find `pwd` -type f -mmin -300 -ls
-echo "*=*=*=*=*=*=*=*=*=*=*=*=*=*"
+find `pwd` -type f -mmin -300 -ls | grep -v /\.git* > last-change-files.txt
+echo "------------------------"
+echo "|     Actualizando     |"
+echo "|          GIT         |"
+echo "------------------------"
 git add -A 
 git commit -m "$text_commit"
 git push
